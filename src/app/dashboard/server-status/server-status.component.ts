87fbd74967c8e5b0e0d188663725b60593e7a3de
rajanga <ganeshbabu.rajan@rabobank.nl>
2025-02-13 10:55:40 +0100

    life cycle with destroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'offline';
+export class ServerStatusComponent implements OnInit, AfterViewInit, OnDestroy {
+  currentStatus: | 'online' | 'offline' | 'unknown' = 'online';
+  private intervel?: ReturnType<typeof setInterval>;
+  constructor() { }
+
+  ngOnInit() {
+    console.log('NG ON INIT');
+    this.intervel = setInterval(() => {
+      const rand = this.getRandom(1, 3);
+      if (rand === 1) {
+        this.currentStatus = 'online'
+      } else if (rand === 2) {
+        this.currentStatus = 'offline'
+      } else if (rand === 3) {
+        this.currentStatus = 'unknown'
+      }
+    }, 5000)
+  }
+
+  ngAfterViewInit(): void {
+    console.log('NG AFTER VIEW INIT');
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.intervel);
+    console.log('NG ON DESTROY');
+  }
+
+  getRandom(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
